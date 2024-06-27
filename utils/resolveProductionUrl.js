72280ddbe55fa082d95a586c3f159253899a4bd1
Sam Goddard <hello@samgoddard.co.uk>
2024-06-27 21:38:18 +0100

    preview mode enabled

--- utils/resolveProductionUrl.js
+++ utils/resolveProductionUrl.js
@@ -1,15 +1,17 @@
 // All Slugs go here...
 const globalSlugs = {
   home: '/',
-  work: '/work',
-  // etc: '/etc'
+  about: '/about',
+  network: '/network',
+  training: '/training',
+  resources: '/resources'
 }
 
 const secret = process.env.SANITY_STUDIO_SANITY_PREVIEW_SECRET
 export const getGlobalSlug = (slug) => globalSlugs[slug]
 
 // Change remote url...
-const remoteURL = 'https://your-live-site.com'
+const remoteURL = 'https://vita-foundation.vercel.app'
 const localURL = 'http://localhost:3000'
 export const previewURL = window.location.hostname === 'localhost' ? localURL : remoteURL
 
